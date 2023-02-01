349d3dc9382bbbfd9ff64ba07cdfd3f53497ef9c
chenjujun <0431cjj@163.com>
2023-02-01 19:19:05 +0800

    5.1

--- src/main/java/com/springboot/demo/SpringBootDemoApplication.java
+++ src/main/java/com/springboot/demo/SpringBootDemoApplication.java
@@ -2,6 +2,7 @@ package com.springboot.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SpringBootDemoApplication {
@@ -10,7 +11,14 @@ public class SpringBootDemoApplication {
         SpringApplication application = new SpringApplication(SpringBootDemoApplication.class);
         //4.8.3 自动重启
         //关闭shudown hook,测试devTools自动重启是否失效
-        application.setRegisterShutdownHook(false);
+//        application.setRegisterShutdownHook(false);
+
+        //5.1 SpringApplication,额外的日志记录
+//        application.setLogStartupInfo(false);
+
+        //5.1.2 惰性初始化
+//        application.setLazyInitialization(true);
+
         application.run(args);
     }
 
