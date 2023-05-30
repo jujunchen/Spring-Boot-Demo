7fd5a4ddf727d09b5546cff1c8503b9bb19f85c3
chenjujun <0431cjj@163.com>
2023-05-30 22:14:38 +0800

    3.1.0 spring boot

--- src/main/java/com/springboot/demo/HelloController.java
+++ src/main/java/com/springboot/demo/HelloController.java
@@ -1,18 +1,11 @@
 package com.springboot.demo;
 
-import org.springframework.boot.task.TaskSchedulerBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
-
 @RestController
 public class HelloController {
-
-// 5.7 任务执行和调度
-//    @Resource
-//    private TaskSchedulerBuilder taskSchedulerBuilder;
 
     @GetMapping("index")
     public String index(@RequestParam("id") String d) {
--- src/main/java/com/springboot/demo/SpringBootDemoApplication.java
+++ src/main/java/com/springboot/demo/SpringBootDemoApplication.java
@@ -2,23 +2,12 @@ package com.springboot.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(SpringBootDemoApplication.class);
-        //4.8.3 自动重启
-        //关闭shudown hook,测试devTools自动重启是否失效
-//        application.setRegisterShutdownHook(false);
-
-        //5.1 SpringApplication,额外的日志记录
-//        application.setLogStartupInfo(false);
-
-        //5.1.2 惰性初始化
-//        application.setLazyInitialization(true);
-
         application.run(args);
     }
 
