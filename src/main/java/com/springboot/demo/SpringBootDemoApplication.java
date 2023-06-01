48f960c4c4d42d7c49a48c832e05506f0188f825
chenjujun <0431cjj@163.com>
2023-06-01 09:38:26 +0800

    3.1.0 spring boot

--- src/main/java/com/springboot/demo/SpringBootDemoApplication.java
+++ src/main/java/com/springboot/demo/SpringBootDemoApplication.java
@@ -6,9 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(SpringBootDemoApplication.class);
-        application.run(args);
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringBootDemoApplication.class);
     }
 
 }
